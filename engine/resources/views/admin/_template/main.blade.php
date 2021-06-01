019e5d87f18b2d0b4aa622dcb12f6c7ec8bfe18a
yosepkandiyas <yosep.kandiyas.id@gmail.com>
2021-06-01 11:59:15 +0700

    change directory 2

--- engine/resources/views/admin/_template/main.blade.php
+++ engine/resources/views/admin/_template/main.blade.php
@@ -30,7 +30,7 @@
     @yield('css')
     <!-- Fonts and OneUI framework -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400italic,600,700%7COpen+Sans:300,400,400italic,600,700">
-    <link rel="stylesheet" id="css-main" href="{{ url('public/assets/dashboard/css/oneui.min.css') }}">
+    <link rel="stylesheet" id="css-main" href="{{ url('assets/dashboard/css/oneui.min.css') }}">
 
     <!-- You can include a specific file from css/themes/ folder to alter the default color theme of the template. eg: -->
     <!-- <link rel="stylesheet" id="css-theme" href="assets/css/themes/amethyst.min.css"> -->
