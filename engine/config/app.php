b72a14586513c1c96a666c653e9e0e944deda52f
yosepkandiyas <yosep.kandiyas.id@gmail.com>
2021-07-10 17:52:28 +0700

    fix config App

--- engine/config/app.php
+++ engine/config/app.php
@@ -168,7 +168,6 @@ return [
         Jenssegers\Agent\AgentServiceProvider::class,
         Maatwebsite\Excel\ExcelServiceProvider::class,
         Gloudemans\Shoppingcart\ShoppingcartServiceProvider::class,
-        Shetabit\Visitor\Provider\VisitorServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -234,7 +233,6 @@ return [
         'Agent' => Jenssegers\Agent\Facades\Agent::class,
         'Excel' => Maatwebsite\Excel\Facades\Excel::class,
         'Cart' => Gloudemans\Shoppingcart\Facades\Cart::class,
-        'Visitor' => Shetabit\Visitor\Facade\Visitor::class,
     ],
 
 ];
