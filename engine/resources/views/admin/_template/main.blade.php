eabe9c190568756d247f7f4febbc4e52d60c13c4
yosepkandiyas <yosep.kandiyas.id@gmail.com>
2021-07-16 02:47:10 +0700

    test pagespeed

--- engine/resources/views/admin/_template/main.blade.php
+++ engine/resources/views/admin/_template/main.blade.php
@@ -7,116 +7,35 @@
 
     <title>Admin - @yield('title')</title>
 
-    <meta name="description" content="OneUI - Bootstrap 4 Admin Template &amp; UI Framework created by pixelcave and published on Themeforest">
-    <meta name="author" content="pixelcave">
-    <meta name="robots" content="noindex, nofollow">
-
-    <!-- Open Graph Meta -->
-    <meta property="og:title" content="OneUI - Bootstrap 4 Admin Template &amp; UI Framework">
-    <meta property="og:site_name" content="OneUI">
-    <meta property="og:description" content="OneUI - Bootstrap 4 Admin Template &amp; UI Framework created by pixelcave and published on Themeforest">
-    <meta property="og:type" content="website">
-    <meta property="og:url" content="">
-    <meta property="og:image" content="">
-
-    <!-- Icons -->
-    <!-- The following icons can be replaced with your own, they are used by desktop and mobile browsers -->
     <link rel="shortcut icon" href="{{ url('assets/dashboard/media/favicons/favicon.png')}} ">
     <link rel="icon" type="image/png" sizes="192x192" href="{{ url('assets/dashboard/media/favicons/favicon-192x192.png')}}">
     <link rel="apple-touch-icon" sizes="180x180" href="{{ url('assets/dashboard/media/favicons/apple-touch-icon-180x180.png')}}">
-    <!-- END Icons -->
 
-    <!-- Stylesheets -->
     @yield('css')
-    <!-- Fonts and OneUI framework -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400italic,600,700%7COpen+Sans:300,400,400italic,600,700">
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
     <link rel="stylesheet" id="css-main" href="{{ url('assets/dashboard/css/style.css') }}">
-
-    <link rel="stylesheet" id="css-main" href="https://cdn.jsdelivr.net/gh/yosepkandiyas/cdn/oneui/css/oneui.min.css">
-
-
-    <!-- You can include a specific file from css/themes/ folder to alter the default color theme of the template. eg: -->
-    <!-- <link rel="stylesheet" id="css-theme" href="assets/css/themes/amethyst.min.css"> -->
-    <!-- END Stylesheets -->
+    <link rel="stylesheet" id="css-main" href="{{ url('assets/dashboard/css/oneui.min.css') }}">
 </head>
 
 <body>
-    <!-- Page Container -->
-    <!--
-            Available classes for #page-container:
-
-        GENERIC
-
-            'enable-cookies'                            Remembers active color theme between pages (when set through color theme helper Template._uiHandleTheme())
-
-        SIDEBAR & SIDE OVERLAY
-
-            'sidebar-r'                                 Right Sidebar and left Side Overlay (default is left Sidebar and right Side Overlay)
-            'sidebar-mini'                              Mini hoverable Sidebar (screen width > 991px)
-            'sidebar-o'                                 Visible Sidebar by default (screen width > 991px)
-            'sidebar-o-xs'                              Visible Sidebar by default (screen width < 992px)
-            'sidebar-dark'                              Dark themed sidebar
-
-            'side-overlay-hover'                        Hoverable Side Overlay (screen width > 991px)
-            'side-overlay-o'                            Visible Side Overlay by default
-
-            'enable-page-overlay'                       Enables a visible clickable Page Overlay (closes Side Overlay on click) when Side Overlay opens
-
-            'side-scroll'                               Enables custom scrolling on Sidebar and Side Overlay instead of native scrolling (screen width > 991px)
-
-        HEADER
-
-            ''                                          Static Header if no class is added
-            'page-header-fixed'                         Fixed Header
-
-        HEADER STYLE
-
-            ''                                          Light themed Header
-            'page-header-dark'                          Dark themed Header
-
-        MAIN CONTENT LAYOUT
-
-            ''                                          Full width Main Content if no class is added
-            'main-content-boxed'                        Full width Main Content with a specific maximum width (screen width > 1200px)
-            'main-content-narrow'                       Full width Main Content with a percentage width (screen width > 1200px)
-        -->
     <div id="page-container" class="sidebar-o sidebar-dark enable-page-overlay side-scroll page-header-fixed">
-        <!-- Sidebar -->
-        <!--
-                Sidebar Mini Mode - Display Helper classes
-
-                Adding 'smini-hide' class to an element will make it invisible (opacity: 0) when the sidebar is in mini mode
-                Adding 'smini-show' class to an element will make it visible (opacity: 1) when the sidebar is in mini mode
-                    If you would like to disable the transition animation, make sure to also add the 'no-transition' class to your element
-
-                Adding 'smini-hidden' to an element will hide it when the sidebar is in mini mode
-                Adding 'smini-visible' to an element will show it (display: inline-block) only when the sidebar is in mini mode
-                Adding 'smini-visible-block' to an element will show it (display: block) only when the sidebar is in mini mode
-            -->
         <nav id="sidebar" aria-label="Main Navigation">
             @include('admin._template.sidebar')
         </nav>
-        <!-- END Sidebar -->
 
-        <!-- Header -->
         <header id="page-header">
             @include('admin._template.header')
         </header>
-        <!-- END Header -->
 
-        <!-- Main Container -->
         <main id="main-container">
             @yield('content')
         </main>
-        <!-- END Main Container -->
 
     </div>
-    <!-- END Page Container -->
 
-    <!-- OneUI JS -->
-    <script src="https://cdn.jsdelivr.net/gh/yosepkandiyas/cdn/oneui/js/oneui.core.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/gh/yosepkandiyas/cdn/oneui/js/oneui.app.min.js"></script>
+    <script src="{{ url('assets/dashboard/js/oneui.core.min.js') }}"></script>
+    <script src="{{ url('assets/dashboard/js/oneui.app.min.js') }}"></script>
     <script src="{{url('assets/dashboard/js/plugin/bootstrap-notify/bootstrap-notify.min.js')}}"></script>
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 
@@ -124,21 +43,25 @@
         @if(session('status'))
         $.notify({
             title: '<strong>SUKSES</strong><br>',
-            message: '{{ session('status') }}'
+            message: '{{ session('
+            status ') }}'
         }, {
             type: 'success'
         });
         @endif
+
     </script>
     <script>
         @if(session('error'))
         $.notify({
             title: '<strong>ERROR</strong><br>',
-            message: '{{ session('error') }}'
+            message: '{{ session('
+            error ') }}'
         }, {
             type: 'danger'
         });
         @endif
+
     </script>
 
     <script>
@@ -152,6 +75,7 @@
         });
         @endforeach
         @endif
+
     </script>
     @yield('js')
 </body>
--- engine/resources/views/mobile/_template/main.blade.php
+++ engine/resources/views/mobile/_template/main.blade.php
@@ -24,7 +24,7 @@
     <link rel="apple-touch-icon" href="{{url('favicon.svg')}}">
     <meta name="theme-color" content="#4285f4">
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/yosepkandiyas/cdn/style.min.css">
+    <link rel="stylesheet" href="{{url('assets/website/css/atyle.min.css')}}">
     <title>Gudang Reseller - @yield('title')</title>
 </head>
 
@@ -46,7 +46,7 @@
     <div style="height:65px;"></div>
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 
